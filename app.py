eddcf0d740a573f1138d8fca6c8804a15745e509
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2025-01-03 23:35:39 +0700

    adding gunicorn for render hosting

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 from flask import Flask, render_template, request, jsonify
-import cv2
+import cv2 
 import numpy as np
-import logging
 from prediction import predict
-from flask_cors import CORS
+from flask_cors import CORS 
+
 app = Flask(__name__)
 CORS(app)
+
 @app.route('/upload', methods=['POST'])
 def upload():
-    if request.method == 'POST':
-        # get image from request
+    if request.method == 'POST': 
         img = request.files['img']
-        print(img)
         img = cv2.imdecode(np.fromstring(img.read(), np.uint8), cv2.IMREAD_COLOR)
         model_path = 'models/model.pkl'
         scaler_path = 'models/scaler.pkl'
         categories = ["blackhead", "whitehead", "pustula", "nodule"]
 
-        # make prediction
         label, confidence, feat = predict(img, model_path, scaler_path, categories)
         if label:
             return jsonify({
@@ -28,6 +26,6 @@ def upload():
         else:
             return jsonify({'error': 'Prediction failed'}), 500
 
-
+# Use this to ensure it runs with gunicorn in production
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
